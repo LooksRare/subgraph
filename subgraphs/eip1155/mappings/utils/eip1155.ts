f72490dfa384df7818af18b12dbc8af0b06b095f
hexacroes <90090323+hexacroes@users.noreply.github.com>
2021-09-18 13:25:08 +0200

    feat: EIP-1155 Subgraph (#2)
    * feat: EIP-1155 Subgraph
    
    * fix: Subgraph configuration
    
    * chore: Update startBlock
    
    * chore: Comments

---
+++ subgraphs/eip1155/mappings/utils/eip1155.ts
@@ -0,0 +1,36 @@
+/* eslint-disable prefer-const */
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { EIP1155 } from "../../generated/EIP1155/EIP1155";
+
+export function fetchName(address: Address): string {
+  let contract = EIP1155.bind(address);
+
+  let nameResult = contract.try_name();
+  if (!nameResult.reverted) {
+    return nameResult.value;
+  }
+
+  return "unknown";
+}
+
+export function fetchSymbol(address: Address): string {
+  let contract = EIP1155.bind(address);
+
+  let symbolResult = contract.try_symbol();
+  if (!symbolResult.reverted) {
+    return symbolResult.value;
+  }
+
+  return "unknown";
+}
+
+export function fetchURI(address: Address, tokenID: BigInt): string {
+  let contract = EIP1155.bind(address);
+
+  let uriResult = contract.try_uri(tokenID);
+  if (!uriResult.reverted) {
+    return uriResult.value;
+  }
+
+  return null;
+}
