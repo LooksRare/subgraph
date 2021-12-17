ef09a861df3add9524258addd0f1a67ba3cd580d
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2021-12-17 16:48:02 +0900

    chore: Added currency to list

--- subgraphs/airdrop/mappings/utils/index.ts
+++ subgraphs/airdrop/mappings/utils/index.ts
@@ -5,7 +5,8 @@ export let currencies: string[] = [
   "0x0000000000000000000000000000000000000000", // ETH
   "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
   "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
-  "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+  "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI,
+  "0x3845badAde8e6dFF049820680d1F14bD3903a5d0" // SAND
 ];
 
 export function toBigDecimal(quantity: BigInt, decimals: i32 = 18): BigDecimal {
