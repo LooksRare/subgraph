5b050aac288142e97f945e4303cbfff0449a7ab9
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2021-12-22 09:44:07 +0900

    feat: Airdrop updates (#16)
    * chore: Added UniswapV2 ABI
    
    * chore: Added currency to list
    
    * feat: Added support for conversion through uniswapV2
    
    * refactor: Merged ExchangeRate and Currency
    
    * fix: Fixed error in logic
    
    * fix: Fixed for decimals
    
    * fix: Fixed adjustment place in code
    
    * refactor: Refactored for etherAddresses array
    
    * chore: Adjusted for lint error
    
    * fix: Fixed for lowercase in SAND address
    
    * chore: Removed TODO

--- subgraphs/airdrop/mappings/index.ts
+++ subgraphs/airdrop/mappings/index.ts
@@ -2,87 +2,139 @@
 import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 import { Balance, Currency, User } from "../generated/schema";
 import { AtomicMatch_Call } from "../generated/WyvernExchange/WyvernExchange";
+
+import { currencies, etherAddresses, ONE_BD, ONE_BI, toBigDecimal, ZERO_BD, ZERO_BI } from "./utils";
 import { fetchDecimals, fetchName, fetchSymbol } from "./utils/erc20";
-import { currencies, toBigDecimal } from "./utils";
+import { getPrice } from "./utils/getPrice";
+
+const END_BLOCK = BigInt.fromString("13812868");
+
+function initializeUser(user: string, currency: string, timestamp: BigInt): User {
+  const newUser = new User(user);
+  newUser.totalBuyTransactions = ZERO_BI;
+  newUser.totalSellTransactions = ZERO_BI;
+  newUser.totalVolumeInETH = ZERO_BD;
+  newUser.firstTradeAt = timestamp;
+  newUser.firstTradeWith = currency;
+  newUser.lastTradeAt = ZERO_BI;
+  newUser.lastTradeWith = "";
+  return newUser;
+}
+
+function initializeUserBalance(user: string, currency: string): Balance {
+  const balance = new Balance(user + "-" + currency);
+  balance.currency = currency;
+  balance.user = user;
+  balance.volume = ZERO_BD;
+  balance.updatedAt = ZERO_BI;
+  return balance;
+}
 
 export function handleAtomicMatch(call: AtomicMatch_Call): void {
-  // If the currency used for the trade isn't whitelisted, skipping.
+  // Stop indexing after end block
+  if (call.block.number > END_BLOCK) {
+    return;
+  }
+
+  // If the currency used for the trade isn't in the whitelist, skip.
   if (!currencies.includes(call.inputs.addrs[6].toHex())) {
     return;
   }
 
-  // Currency
+  // 1. Currency
   let currency = Currency.load(call.inputs.addrs[6].toHex());
   if (currency === null) {
     currency = new Currency(call.inputs.addrs[6].toHex());
     currency.name = fetchName(call.inputs.addrs[6]);
     currency.symbol = fetchSymbol(call.inputs.addrs[6]);
     currency.decimals = fetchDecimals(call.inputs.addrs[6]);
-    currency.totalTrades = BigInt.zero();
-    currency.volume = BigDecimal.zero();
-    currency.save();
+    currency.totalTrades = ZERO_BI;
+    currency.volume = ZERO_BD;
+    currency.priceOfOneETH = ONE_BD;
+    currency.updatedAt = ZERO_BI;
+    if (!etherAddresses.includes(currency.id)) {
+      currency.minPriceOfOneETH = BigDecimal.fromString("9999999999"); // Arbitrary large number
+      currency.maxPriceOfOneETH = ZERO_BD;
+    } else {
+      currency.minPriceOfOneETH = ONE_BD;
+      currency.maxPriceOfOneETH = ONE_BD;
+    }
   }
-  currency.totalTrades = currency.totalTrades.plus(BigInt.fromI32(1));
+
+  currency.totalTrades = currency.totalTrades.plus(ONE_BI);
   currency.volume = currency.volume.plus(toBigDecimal(call.inputs.uints[4], currency.decimals.toI32()));
+
+  let priceOfOneETH = currency.priceOfOneETH;
+  let adjustedCurrencyVolume = toBigDecimal(call.inputs.uints[4], currency.decimals.toI32());
+
+  // Exclude if traded currency is WETH/ETH
+  if (!etherAddresses.includes(currency.id)) {
+    // Refresh only if not updated for 1 hour
+    if (currency.updatedAt.plus(BigInt.fromString("3600")) < call.block.timestamp) {
+      priceOfOneETH = getPrice(currency.id, currency.decimals.toI32());
+
+      currency.priceOfOneETH = priceOfOneETH;
+
+      if (priceOfOneETH > currency.maxPriceOfOneETH) {
+        currency.maxPriceOfOneETH = priceOfOneETH;
+      }
+
+      if (priceOfOneETH < currency.minPriceOfOneETH) {
+        currency.minPriceOfOneETH = priceOfOneETH;
+      }
+
+      currency.updatedAt = call.block.timestamp;
+    }
+    adjustedCurrencyVolume = adjustedCurrencyVolume.div(priceOfOneETH);
+  }
+
   currency.save();
 
-  // Taker a.k.a. Buyer
-  let taker = User.load(call.inputs.addrs[1].toHex());
-  if (taker === null) {
-    taker = new User(call.inputs.addrs[1].toHex());
-    taker.totalBuy = BigInt.zero();
-    taker.totalSell = BigInt.zero();
-    taker.firstTradeAt = call.block.timestamp;
-    taker.firstTradeWith = currency.id;
-    taker.lastTradeAt = call.block.timestamp;
-    taker.lastTradeWith = currency.id;
-    taker.save();
+  // 2. Buyer
+  // 2.1 Buyer
+  let buyer = User.load(call.inputs.addrs[1].toHex());
+  if (buyer === null) {
+    buyer = initializeUser(call.inputs.addrs[1].toHex(), currency.id, call.block.timestamp);
   }
-  taker.totalBuy = taker.totalBuy.plus(BigInt.fromI32(1));
-  taker.lastTradeAt = call.block.timestamp;
-  taker.lastTradeWith = currency.id;
-  taker.save();
 
-  let takerBalance = Balance.load(call.inputs.addrs[1].toHex() + "-" + call.inputs.addrs[6].toHex());
-  if (takerBalance === null) {
-    takerBalance = new Balance(call.inputs.addrs[1].toHex() + "-" + call.inputs.addrs[6].toHex());
-    takerBalance.currency = currency.id;
-    takerBalance.user = taker.id;
-    takerBalance.volume = BigDecimal.zero();
-    takerBalance.updatedAt = call.block.timestamp;
-    takerBalance.save();
-  }
-  takerBalance.volume = takerBalance.volume.plus(toBigDecimal(call.inputs.uints[4], currency.decimals.toI32()));
-  takerBalance.updatedAt = call.block.timestamp;
-  takerBalance.save();
+  buyer.totalBuyTransactions = buyer.totalBuyTransactions.plus(ONE_BI);
+  buyer.lastTradeAt = call.block.timestamp;
+  buyer.lastTradeWith = currency.id;
+  buyer.totalVolumeInETH = buyer.totalVolumeInETH.plus(adjustedCurrencyVolume);
+  buyer.save();
 
-  // Maker a.k.a. Seller
-  let maker = User.load(call.inputs.addrs[8].toHex());
-  if (maker === null) {
-    maker = new User(call.inputs.addrs[8].toHex());
-    maker.totalBuy = BigInt.zero();
-    maker.totalSell = BigInt.zero();
-    maker.firstTradeAt = call.block.timestamp;
-    maker.firstTradeWith = currency.id;
-    maker.lastTradeAt = call.block.timestamp;
-    maker.lastTradeWith = currency.id;
-    maker.save();
-  }
-  maker.totalSell = maker.totalSell.plus(BigInt.fromI32(1));
-  maker.lastTradeAt = call.block.timestamp;
-  maker.lastTradeWith = currency.id;
-  maker.save();
+  // 2.2 Buyer's balance in the currency
+  let buyerBalance = Balance.load(call.inputs.addrs[1].toHex() + "-" + call.inputs.addrs[6].toHex());
 
-  let makerBalance = Balance.load(call.inputs.addrs[8].toHex() + "-" + call.inputs.addrs[6].toHex());
-  if (makerBalance === null) {
-    makerBalance = new Balance(call.inputs.addrs[8].toHex() + "-" + call.inputs.addrs[6].toHex());
-    makerBalance.currency = currency.id;
-    makerBalance.user = maker.id;
-    makerBalance.volume = BigDecimal.zero();
-    makerBalance.updatedAt = call.block.timestamp;
-    makerBalance.save();
+  if (buyerBalance === null) {
+    buyerBalance = initializeUserBalance(call.inputs.addrs[1].toHex(), call.inputs.addrs[6].toHex());
   }
-  makerBalance.volume = makerBalance.volume.plus(toBigDecimal(call.inputs.uints[4], currency.decimals.toI32()));
-  makerBalance.updatedAt = call.block.timestamp;
-  makerBalance.save();
+
+  buyerBalance.volume = buyerBalance.volume.plus(toBigDecimal(call.inputs.uints[4], currency.decimals.toI32()));
+  buyerBalance.updatedAt = call.block.timestamp;
+  buyerBalance.save();
+
+  // 3. Seller
+  // 3.1 Seller
+  let seller = User.load(call.inputs.addrs[8].toHex());
+  if (seller === null) {
+    seller = initializeUser(call.inputs.addrs[8].toHex(), currency.id, call.block.timestamp);
+  }
+
+  seller.totalSellTransactions = seller.totalSellTransactions.plus(ONE_BI);
+  seller.totalVolumeInETH = seller.totalVolumeInETH.plus(adjustedCurrencyVolume);
+  seller.lastTradeAt = call.block.timestamp;
+  seller.lastTradeWith = currency.id;
+  seller.save();
+
+  // 3.2 Seller's balance
+  let sellerBalance = Balance.load(call.inputs.addrs[8].toHex() + "-" + call.inputs.addrs[6].toHex());
+  if (sellerBalance === null) {
+    sellerBalance = initializeUserBalance(call.inputs.addrs[8].toHex(), call.inputs.addrs[6].toHex());
+    sellerBalance.save();
+  }
+
+  sellerBalance.volume = sellerBalance.volume.plus(toBigDecimal(call.inputs.uints[4], currency.decimals.toI32()));
+  sellerBalance.updatedAt = call.block.timestamp;
+  sellerBalance.save();
 }
