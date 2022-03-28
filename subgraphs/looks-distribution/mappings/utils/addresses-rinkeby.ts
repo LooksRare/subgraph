8c90a5adaa2dbf50d07a9c64c25ee5e68151ee47
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-03-28 18:15:11 +0900

    chore: Adjusted Rinkeby address

--- subgraphs/looks-distribution/mappings/utils/addresses-rinkeby.ts
+++ subgraphs/looks-distribution/mappings/utils/addresses-rinkeby.ts
@@ -1,4 +1,4 @@
 import { Address } from "@graphprotocol/graph-ts";
 
-export const AGGREGATOR_ADDRESS = Address.fromString("0xb6d18017d4fac74b855f163be36f2210b26a8a48");
+export const AGGREGATOR_ADDRESS = Address.fromString("0x5b5A702939bF53595dD0C14488528e05e1e8C4A5");
 export const FEE_SHARING_ADDRESS = Address.fromString("0xf32e6141d54512814fb94584fc17baaf0c1203de");
