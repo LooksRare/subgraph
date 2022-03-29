f99bcb0b86e00248ea83fd04c583ddbdc02322cc
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-03-29 11:31:24 +0900

    chore: Adjusted Rinkeby/Mainnet addresses for Aggregator (#45)
    * chore: Adjusted Rinkeby address
    
    * chore: Adjusted addresses for mainnet

--- subgraphs/looks-distribution/mappings/utils/addresses-mainnet.ts
+++ subgraphs/looks-distribution/mappings/utils/addresses-mainnet.ts
@@ -1,4 +1,4 @@
 import { Address } from "@graphprotocol/graph-ts";
 
-export const AGGREGATOR_ADDRESS = Address.fromString("0x0000000000000000000000000000000000000000");
+export const AGGREGATOR_ADDRESS = Address.fromString("0x3ab16af1315dc6c95f83cbf522fecf98d00fd9ba");
 export const FEE_SHARING_ADDRESS = Address.fromString("0xbcd7254a1d759efa08ec7c3291b2e85c5dcc12ce");
--- subgraphs/looks-distribution/mappings/utils/addresses-rinkeby.ts
+++ subgraphs/looks-distribution/mappings/utils/addresses-rinkeby.ts
@@ -1,4 +1,4 @@
 import { Address } from "@graphprotocol/graph-ts";
 
-export const AGGREGATOR_ADDRESS = Address.fromString("0xb6d18017d4fac74b855f163be36f2210b26a8a48");
+export const AGGREGATOR_ADDRESS = Address.fromString("0x5b5a702939bf53595dd0c14488528e05e1e8c4a5");
 export const FEE_SHARING_ADDRESS = Address.fromString("0xf32e6141d54512814fb94584fc17baaf0c1203de");
