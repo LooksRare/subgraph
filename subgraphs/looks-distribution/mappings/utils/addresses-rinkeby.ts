698fb2a5aebef8108b59e79043c00ab7ee87c863
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-03-29 09:26:39 +0900

    chore: Adjusted addresses for mainnet

--- subgraphs/looks-distribution/mappings/utils/addresses-rinkeby.ts
+++ subgraphs/looks-distribution/mappings/utils/addresses-rinkeby.ts
@@ -1,4 +1,4 @@
 import { Address } from "@graphprotocol/graph-ts";
 
-export const AGGREGATOR_ADDRESS = Address.fromString("0x5b5A702939bF53595dD0C14488528e05e1e8C4A5");
+export const AGGREGATOR_ADDRESS = Address.fromString("0x5b5a702939bf53595dd0c14488528e05e1e8c4a5");
 export const FEE_SHARING_ADDRESS = Address.fromString("0xf32e6141d54512814fb94584fc17baaf0c1203de");
