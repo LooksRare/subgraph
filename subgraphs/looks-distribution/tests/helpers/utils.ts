e4f9842b866aef61dfb45bbccd2e3d0e6ebe9453
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-04-26 12:59:02 +0900

    test: TradingRewardsDistributor unit test

---
+++ subgraphs/looks-distribution/tests/helpers/utils.ts
@@ -0,0 +1,34 @@
+/* eslint-disable prefer-const */
+import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { newMockEvent } from "matchstick-as";
+import { RewardsClaim } from "../../generated/TradingRewardsDistributor/TradingRewardsDistributor";
+
+/**
+ * @param user
+ * @param rewardRound
+ * @param amount
+ * @returns RewardsClaim Event
+ */
+export function createRewardsClaimEvent(user: Address, rewardRound: BigInt, amount: BigInt): RewardsClaim {
+  let mockEvent = newMockEvent();
+  let newRewardsClaimEvent = new RewardsClaim(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    mockEvent.parameters
+  );
+
+  newRewardsClaimEvent.parameters = [];
+  let userParam = new ethereum.EventParam("user", ethereum.Value.fromAddress(user));
+  let rewardRoundParam = new ethereum.EventParam("rewardRound", ethereum.Value.fromSignedBigInt(rewardRound));
+  let amountParam = new ethereum.EventParam("amount", ethereum.Value.fromSignedBigInt(amount));
+
+  newRewardsClaimEvent.parameters.push(userParam);
+  newRewardsClaimEvent.parameters.push(rewardRoundParam);
+  newRewardsClaimEvent.parameters.push(amountParam);
+
+  return newRewardsClaimEvent;
+}
---
+++ subgraphs/looks-distribution/tests/tradingRewardsDistributor.test.ts
@@ -0,0 +1,40 @@
+/* eslint-disable prefer-const */
+import { Address } from "@graphprotocol/graph-ts";
+import { assert, clearStore, log, test } from "matchstick-as/assembly/index";
+
+import { createRewardsClaimEvent } from "./helpers/utils";
+import { handleTradingRewardsClaim } from "../mappings";
+import { parseEther, ONE_BI, TWO_BI } from "../../../helpers/utils";
+import { User } from "../generated/schema";
+
+test("RewardsClaim", () => {
+  let userAddress = Address.fromString("0x0000000000000000000000000000000000000001");
+  let amountInLOOKS = 20; // 20 LOOKS
+  let amountClaimed = parseEther(amountInLOOKS);
+  let totalAmountClaimed = amountInLOOKS;
+
+  let newRewardsClaimEvent = createRewardsClaimEvent(userAddress, ONE_BI, amountClaimed);
+  handleTradingRewardsClaim(newRewardsClaimEvent);
+
+  let user = User.load(userAddress.toHex());
+  if (user !== null) {
+    assert.stringEquals(user.tradingRewardsAmount.toString(), amountInLOOKS.toString());
+  } else {
+    log.warning("User doesn't exist", []);
+  }
+
+  amountInLOOKS = 50; // 50 LOOKS
+  amountClaimed = parseEther(amountInLOOKS);
+  totalAmountClaimed += amountInLOOKS;
+  newRewardsClaimEvent = createRewardsClaimEvent(userAddress, TWO_BI, amountClaimed);
+  handleTradingRewardsClaim(newRewardsClaimEvent);
+
+  user = User.load(userAddress.toHex());
+  if (user !== null) {
+    assert.stringEquals(user.tradingRewardsAmount.toString(), totalAmountClaimed.toString());
+  } else {
+    log.warning("User doesn't exist", []);
+  }
+
+  clearStore();
+});
