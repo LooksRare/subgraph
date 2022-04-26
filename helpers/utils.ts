0e5c32612d5dbf1fcdd203375be60cfcd7f34738
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-04-26 12:58:46 +0900

    test: Adjustment helpers for TWO_BI, THREE_BI

--- helpers/utils.ts
+++ helpers/utils.ts
@@ -1,11 +1,11 @@
-/* eslint-disable prefer-const */
 import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 export const ZERO_BI = BigInt.zero();
 export const ONE_BI = BigInt.fromI32(1);
+export const TWO_BI = BigInt.fromI32(2);
+export const THREE_BI = BigInt.fromI32(3);
 export const ZERO_BD = BigDecimal.zero();
 export const ONE_BD = BigDecimal.fromString("1");
-
 export const ZERO_ADDRESS = Address.fromString("0x0000000000000000000000000000000000000000");
 
 export function toBigDecimal(quantity: BigInt, decimals: i32 = 18): BigDecimal {
