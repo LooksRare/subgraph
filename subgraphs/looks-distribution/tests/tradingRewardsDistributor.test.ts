90f85ddfcca9e8528e6e2b2a80617c74b2fe22c1
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-04-26 15:46:54 +0900

    test: Updates

--- subgraphs/looks-distribution/tests/tradingRewardsDistributor.test.ts
+++ subgraphs/looks-distribution/tests/tradingRewardsDistributor.test.ts
@@ -4,8 +4,8 @@ import { assert, clearStore, log, test } from "matchstick-as/assembly/index";
 
 import { createRewardsClaimEvent } from "./helpers/utils";
 import { handleTradingRewardsClaim } from "../mappings";
-import { parseEther, ONE_BI, TWO_BI } from "../../../helpers/utils";
 import { User } from "../generated/schema";
+import { parseEther, ONE_BI, TWO_BI } from "../../../helpers/utils";
 
 test("RewardsClaim", () => {
   let userAddress = Address.fromString("0x0000000000000000000000000000000000000001");
@@ -18,6 +18,7 @@ test("RewardsClaim", () => {
 
   let user = User.load(userAddress.toHex());
   if (user !== null) {
+    assert.bigIntEquals(user.tradingRewardsLastClaimDate, newRewardsClaimEvent.block.timestamp);
     assert.stringEquals(user.tradingRewardsAmount.toString(), amountInLOOKS.toString());
   } else {
     log.warning("User doesn't exist", []);
@@ -26,11 +27,15 @@ test("RewardsClaim", () => {
   amountInLOOKS = 50; // 50 LOOKS
   amountClaimed = parseEther(amountInLOOKS);
   totalAmountClaimed += amountInLOOKS;
+
   newRewardsClaimEvent = createRewardsClaimEvent(userAddress, TWO_BI, amountClaimed);
+  // Adjust block timestamp to make it different
+  newRewardsClaimEvent.block.timestamp = TWO_BI;
   handleTradingRewardsClaim(newRewardsClaimEvent);
 
   user = User.load(userAddress.toHex());
   if (user !== null) {
+    assert.bigIntEquals(user.tradingRewardsLastClaimDate, newRewardsClaimEvent.block.timestamp);
     assert.stringEquals(user.tradingRewardsAmount.toString(), totalAmountClaimed.toString());
   } else {
     log.warning("User doesn't exist", []);
