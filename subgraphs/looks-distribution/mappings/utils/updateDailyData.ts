89131fb4bdbc3398815d4e5bbf391dbccef51ade
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-05-01 10:05:32 +0900

    refactor: File adjustments

--- subgraphs/looks-distribution/mappings/utils/updateDailyData.ts
+++ subgraphs/looks-distribution/mappings/utils/updateDailyData.ts
@@ -1,42 +1,8 @@
 import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 import { initializeDailySnapshot } from "./initializeDailySnapshot";
-import { initializeOverview } from "./initializeOverview";
-import { fetchTotalAmountStakedAggregator, fetchTotalAmountStakedFeeSharing } from "./rpc-calls/fetchTotalAmountStaked";
 import { DailySnapshot, Overview } from "../../generated/schema";
-import { ZERO_BI, ONE_BI } from "../../../../helpers/constants";
-
-export function setupOverviewAndDailySnapshot(timestamp: BigInt): DailySnapshot {
-  const dailyTimestampBigInt = BigInt.fromI32(86400);
-  const dayID = timestamp.div(dailyTimestampBigInt);
-  const dayStartTimestamp = dayID.times(dailyTimestampBigInt);
-  const ID = dayID.toString();
-
-  let overview = Overview.load(ONE_BI.toHex());
-  if (overview === null) {
-    overview = initializeOverview();
-  }
-
-  let dailySnapshot = DailySnapshot.load(ID);
-  if (dailySnapshot === null) {
-    dailySnapshot = initializeDailySnapshot(ID, dayStartTimestamp);
-    dailySnapshot.aggregatorActiveUsers = overview.aggregatorActiveUsers;
-    dailySnapshot.feeSharingActiveUsers = overview.feeSharingActiveUsers;
-    dailySnapshot.aggregatorTotalStakedLOOKS = overview.aggregatorTotalStakedLOOKS;
-    dailySnapshot.feeSharingTotalStakedLOOKS = overview.feeSharingTotalStakedLOOKS;
-
-    if (overview.aggregatorActiveUsers.gt(ZERO_BI)) {
-      overview.aggregatorTotalStakedLOOKS = fetchTotalAmountStakedAggregator();
-    }
-
-    if (overview.feeSharingActiveUsers.gt(ZERO_BI)) {
-      overview.feeSharingTotalStakedLOOKS = fetchTotalAmountStakedFeeSharing().minus(
-        overview.aggregatorTotalStakedLOOKS
-      );
-    }
-    overview.save();
-  }
-  return dailySnapshot;
-}
+import { ONE_BI } from "../../../../helpers/constants";
+import { setupOverviewAndDailySnapshot } from "./setupOverviewAndDailySnapshot";
 
 export function updateDailySnapshotDepositFeeSharing(timestamp: BigInt, amount: BigDecimal): void {
   const dailySnapshot = setupOverviewAndDailySnapshot(timestamp);
