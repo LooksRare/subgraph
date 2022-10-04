9141b01ebf60c0851d9a40ee127dd0d0b8c16069
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-10-04 11:17:13 +0900

    chore: Add dynamic addresses from SDK

--- subgraphs/looks-distribution/mappings/utils/config/addresses-mainnet.ts
+++ subgraphs/looks-distribution/mappings/utils/config/addresses-mainnet.ts
@@ -1,4 +1,5 @@
 import { Address } from "@graphprotocol/graph-ts";
+import { addressesByNetwork } from "@looksrare/sdk";
 
-export const AGGREGATOR_ADDRESS = Address.fromString("0x3ab16af1315dc6c95f83cbf522fecf98d00fd9ba");
-export const FEE_SHARING_ADDRESS = Address.fromString("0xbcd7254a1d759efa08ec7c3291b2e85c5dcc12ce");
+export const AGGREGATOR_ADDRESS = Address.fromString(addressesByNetwork[1].AGGREGATOR_UNISWAP_V3);
+export const FEE_SHARING_ADDRESS = Address.fromString(addressesByNetwork[1].FEE_SHARING_SYSTEM);
--- subgraphs/looks-distribution/mappings/utils/config/addresses-rinkeby.ts
+++ subgraphs/looks-distribution/mappings/utils/config/addresses-rinkeby.ts
@@ -1,4 +1,5 @@
 import { Address } from "@graphprotocol/graph-ts";
+import { addressesByNetwork } from "@looksrare/sdk";
 
-export const AGGREGATOR_ADDRESS = Address.fromString("0x5b5a702939bf53595dd0c14488528e05e1e8c4a5");
-export const FEE_SHARING_ADDRESS = Address.fromString("0xf32e6141d54512814fb94584fc17baaf0c1203de");
+export const AGGREGATOR_ADDRESS = Address.fromString(addressesByNetwork[4].AGGREGATOR_UNISWAP_V3);
+export const FEE_SHARING_ADDRESS = Address.fromString(addressesByNetwork[4].FEE_SHARING_SYSTEM);
