a3224420161bcbdc5e0c46177067cfec1c32cf88
0xJurassicPunk <90075915+0xJurassicPunk@users.noreply.github.com>
2022-10-05 17:26:23 +0900

    fix: Initialization DailySnapshot (#84)
    * fix: Initialization DailySnapshot
    
    * fix: Addresses

--- subgraphs/looks-distribution/mappings/utils/config/addresses-goerli.ts
+++ subgraphs/looks-distribution/mappings/utils/config/addresses-goerli.ts
@@ -1,5 +1,4 @@
 import { Address } from "@graphprotocol/graph-ts";
-import { addressesByNetwork } from "@looksrare/sdk";
 
-export const AGGREGATOR_ADDRESS = Address.fromString(addressesByNetwork[5].AGGREGATOR_UNISWAP_V3);
-export const FEE_SHARING_ADDRESS = Address.fromString(addressesByNetwork[5].FEE_SHARING_SYSTEM);
+export const AGGREGATOR_ADDRESS = Address.fromString("0x63c38b3be3ef075a00a5edaec36f499088c7334c");
+export const FEE_SHARING_ADDRESS = Address.fromString("0x8a29e7b241a32158fd7f267d484199aad8dd2e31");
