811e0f14fd13c51a3f9b4ff569f85ecae6dcbf61
hiroshi <hiroshi@looksrare.org>
2022-12-20 16:39:00 +0000

    feat: Save aggregator daily data

--- subgraphs/aggregator/src/LooksRareV1/index.ts
+++ subgraphs/aggregator/src/LooksRareV1/index.ts
@@ -1,19 +1,38 @@
-import { ONE_BI } from "../../../../helpers/constants";
+import { ONE_BI, ONE_DAY_BI } from "../../../../helpers/constants";
 import { TakerBid } from "../../generated/LooksRareV1/LooksRareExchange";
 import { getOrInitializeAggregator } from "../utils/getOrInitializeAggregator";
 import { getOrInitializeAggregatorByCurrency } from "../utils/getOrInitializeAggregatorByCurrency";
+import { getOrInitializeAggregatorDailyData } from "../utils/getOrInitializeAggregatorDailyData";
+import { getOrInitializeAggregatorDailyDataByCurrency } from "../utils/getOrInitializeAggregatorDailyDataByCurrency";
 
 export function handleTakerBid(event: TakerBid): void {
   const currency = event.params.currency;
   const price = event.params.price.toBigDecimal();
+  const dayID = event.block.timestamp.div(ONE_DAY_BI);
 
+  // 1. Aggregator
   const aggregator = getOrInitializeAggregator();
   aggregator.transactions = aggregator.transactions.plus(ONE_BI);
 
+  // 2. Aggregator by currency
   const aggregatorByCurrency = getOrInitializeAggregatorByCurrency(aggregator, currency);
   aggregatorByCurrency.volume = aggregatorByCurrency.volume.plus(price);
   aggregatorByCurrency.transactions = aggregatorByCurrency.transactions.plus(ONE_BI);
 
+  // 3. Aggregator daily data
+  const aggregatorDailyData = getOrInitializeAggregatorDailyData(dayID, aggregator);
+  aggregatorDailyData.transactions = aggregatorDailyData.transactions.plus(ONE_BI);
+
+  // 4. Aggregator daily data by currency
+  const aggregatorDailyDataByCurrency = getOrInitializeAggregatorDailyDataByCurrency(
+    aggregatorByCurrency,
+    aggregatorDailyData,
+    dayID
+  );
+  aggregatorDailyDataByCurrency.volume = aggregatorDailyDataByCurrency.volume.plus(price);
+
   aggregator.save();
   aggregatorByCurrency.save();
+  aggregatorDailyData.save();
+  aggregatorDailyDataByCurrency.save();
 }
