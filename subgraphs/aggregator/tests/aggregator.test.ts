9ea874ecbfb0ffff868307e7a12e0c09bc7f10e7
hiroshi <hiroshi@looksrare.org>
2022-12-20 17:28:52 +0000

    chore: Address is no longer used

--- subgraphs/aggregator/tests/aggregator.test.ts
+++ subgraphs/aggregator/tests/aggregator.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 import { afterEach, assert, clearStore, describe, test } from "matchstick-as/assembly/index";
 import { createOrderFulfilledEvent, createTakerBidEvent, newLog } from "./helpers/utils";
 import { handleOrderFulfilled } from "../src/Seaport/index";
