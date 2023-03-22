923f95d728adb0c642715dde33414e5522bae29c
hiroshi <hiroshi@looksrare.org>
2023-03-22 11:02:48 +0000

    build: Update LooksRareAggregator address

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -11,7 +11,6 @@ export const ZERO_ADDRESS = Address.fromString("0x000000000000000000000000000000
 export const ONE_ETHER_IN_WEI = BigInt.fromI32(10).pow(18);
 export const ONE_DAY_BI = BigInt.fromI32(86400);
 
-// TODO: Update it with the real address
-export const LOOKSRARE_AGGREGATOR = Address.fromString("0x0000000000000000000000000000000000069420");
+export const LOOKSRARE_AGGREGATOR = Address.fromString("0x00000000005228B791a99a61f36A130d50600106");
 export const LOOKSRARE_AGGREGATOR_SWEEP_EVENT_TOPIC =
   "0x807273efecfbeb7ae7d3a2189d1ed5a7db80074eed86e7d80b10bb925cd1db73";
