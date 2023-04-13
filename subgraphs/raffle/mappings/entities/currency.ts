87a3d0fb8aa32939e96e09f82897c3e24d5dc7a9
hexacroes <90090323+hexacroes@users.noreply.github.com>
2023-04-13 10:26:52 +0900

    feat: Raffle subgraph

---
+++ subgraphs/raffle/mappings/entities/currency.ts
@@ -0,0 +1,20 @@
+import { Address } from "@graphprotocol/graph-ts";
+import { ZERO_ADDRESS } from "../../../../helpers/constants";
+import { Currency } from "../../generated/schema";
+import { fetchDecimals, fetchName, fetchSymbol } from "../utils/ERC20";
+
+export function initializeCurrency(address: Address): Currency {
+  const currency = Currency.load(address.toHex());
+  if (currency === null) {
+    const newCurrency = new Currency(address.toHex());
+    newCurrency.name = fetchName(address);
+    newCurrency.symbol = fetchSymbol(address);
+    newCurrency.decimals = fetchDecimals(address);
+    newCurrency.isAllowed = address.equals(ZERO_ADDRESS) ? true : false;
+    newCurrency.save();
+
+    return newCurrency;
+  }
+
+  return currency;
+}
